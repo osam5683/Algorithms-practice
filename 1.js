898630c0ea9e1fe9b1ac1cbe8a3fd2385eff37bb
osam5683 <akheelossama786@gmail.com>
2022-10-08 13:38:16 +0530

    algos

---
+++ 1.js
@@ -0,0 +1,23 @@
+// Write a program to given input Check whether Even or odd.
+
+
+// let oddeve =(n)=>{
+// if ((n%2)==0){
+//     console.log("Given input is even")
+// }
+// else{
+//     console.log("Given input is odd")
+// }
+// }
+// oddeve(5)
+
+
+// let oddeve =(n)=>{
+//     if((n%2)==0){
+//         console.log('given input is even')
+//     }else{
+//         console.log("given number is odd")
+//     }
+
+// }
+// oddeve(2)
