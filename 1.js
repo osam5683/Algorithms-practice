bf0e7f12ae4a1cef18245d32387f97fe49ba41ce
osam5683 <akheelossama786@gmail.com>
2022-10-08 17:11:47 +0530

    algos

--- 1.js
+++ 1.js
@@ -1,5 +1,4 @@
-// Write a program to given input Check whether Even or odd.
-
+// 1.Write a program to given input Check whether Even or odd.
 
 // let oddeve =(n)=>{
 // if ((n%2)==0){
@@ -11,7 +10,6 @@
 // }
 // oddeve(5)
 
-
 // let oddeve =(n)=>{
 //     if((n%2)==0){
 //         console.log('given input is even')
@@ -21,3 +19,36 @@
 
 // }
 // oddeve(2)
+
+// 2.write a program to generate even and odd number less than N and generate 'N' even and odd numbers
+//
+// let oddeve = (i) => {
+//   for (i = 0; i < 100; i++) {
+//     if (i % 2 == 0) {
+//       console.log(`${i} is even number `);
+//     } else {
+//       console.log(`${i} is odd number `);
+//     }
+//   }
+// };
+// oddeve(100);
+
+// 3) Write a program to decide given N is Prime or not.
+
+// num = 10;
+// let factorfound = true;
+ 
+//   for (i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//       console.log(`${num} is not a prime number`);
+//       factorfound = false;
+//       break;
+//     }
+//   }
+
+// if (factorfound == true) {
+//   console.log(`${num} is a prime number`);
+// }
+
+
+// 4) Write a program to subtract two integers without using Minus (-) operator
